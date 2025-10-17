0360ef8670671f72c12524b7d349c2ef5fd150e9
Lokesh-Jadhav <21f1003306@ds.study.iitm.ac.in>
2025-10-17 10:58:34 +0000

    Update all recent changes

--- app.py
+++ app.py
@@ -312,4 +312,4 @@ def ping():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 8000)), debug=True)
+    app.run(host=os.getenv("HOST", "0.0.0.0"), port=int(os.getenv("PORT", 7860)), debug=True)
